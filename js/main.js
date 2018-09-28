2dedb5bff5652413757bfe0451a54999946c9bcd
Alaa Zorkane <raralazoril@gmail.com>
2018-09-28 16:04:36 +0100

    v1.0.1
    v1.0.1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var es = $("#e-sound")[0];
-$("#snakegasmTitle")
-	.mouseenter(function() {
-		es.play();
+$(function(){
+    $('#about').on('click',function(){
+	  $('#snake-Container,#load').fadeTo("slow",0);
+	  $('.aboutus').css("opacity", 1)
 	});
+});
